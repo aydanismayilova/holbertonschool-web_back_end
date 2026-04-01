84012e218c22285a32a6cbd039fd137da0d8e2cc
aydanismayilova <ismayilovaaydan26@gmail.com>
2026-04-01 17:46:04 +0400

    Add docstring to index function

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+"""Basic Flask application"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Return the index page"""
     return render_template('0-index.html')
 
 if __name__ == "__main__":
