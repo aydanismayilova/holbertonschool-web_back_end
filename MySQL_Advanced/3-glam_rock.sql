5985aa6b8a4f391254c15001c9483139ed3918e6
aydanismayilova <ismayilovaaydan26@gmail.com>
2026-04-22 02:03:43 +0400

    Create 3-glam_rock.sql

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- List Glam rock bands ranked by longevity up to 2024
+SELECT
+    band_name,
+    (IFNULL(split, 2024) - formed) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
