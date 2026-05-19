8d86353eddc00517ea35c6db4a814e55fd819bfe
aydanismayilova <ismayilovaaydan26@gmail.com>
2026-05-20 01:41:31 +0400

    Update 1-app.py

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python3
-"""1-app.py: Flask app with Babel configuration."""
-from flask import Flask, render_template
+"""
+This module is for Babel object instantiation
+"""
+from flask import Flask, request, render_template
 from flask_babel import Babel
 
 
 class Config:
-    """Configuration for Babel."""
+    """
+    This class is for configuring the languages
+    """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
 babel = Babel(app)
 
 
 @app.route('/')
-def index():
-    """Render the 1-index.html template."""
+def home():
+    """
+    Renders the template
+    """
     return render_template('1-index.html')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
-    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
